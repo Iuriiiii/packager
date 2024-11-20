63c6f294b7561aa9e2e02bb7abee9a885a1df2f7
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-11-20 13:12:21 -0300

    fix: string serialization

--- tests/pack-unpack.test.ts
+++ tests/pack-unpack.test.ts
@@ -9,7 +9,7 @@ function randomString(length: number) {
   let result = "";
 
   const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZÁÉÍÓÚÑabcdefghijklmnopqrstuvwxyzáéíóúñ0123456789";
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(
